79bea819c8033908fbff153dd9814760a234ba43
Kcyle <134351158+Kcyle@users.noreply.github.com>
2026-02-27 23:06:48 -0500

    Add sitemap, robots.txt, and Google Search Console verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://xr.umd.edu',
+  integrations: [sitemap()],
   vite: {
     build: {
       cssMinify: true,
